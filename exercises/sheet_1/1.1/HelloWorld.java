c46d9e52aea767d7dbce00bd4bf3084b4a59a9a7
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2010-09-30 13:27:37 +0100

    first lot

---
+++ exercises/sheet_1/1.1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+* <dl>
+* <dt> Purpose:
+* <dd> Simple example program.
+*
+* <dt> Description:
+* <dd> This program prints out the message Hello World.
+* </dl>
+*
+* @author Tim Cowlishaw
+* @version $Date: 2003/09/24 17:22:40 $
+*
+*/
+
+public class HelloWorld {
+  public static void main(String[] args)  {
+    System.out.println("Hello World") ;
+  }
+}
+
---
+++ exercises/sheet_1/1.3/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+* <dl>
+* <dt> Purpose:
+* <dd> Simple example program.
+*
+* <dt> Description:
+* <dd> This program prints out the message Hello World.
+* </dl>
+*
+* @author Tim Cowlishaw
+* @version $Date: 2010/09/30 13:18:00 $
+*
+*/
+
+public class HelloWorld {
+  public static void main(String[] args)  {
+    System.out.print("Tim Cowlishaw");
+    System.out.print("My other programming language is a HASKELL");
+  }
+}
+
