4e7a7c2e8eb7a8e22d03708901bdb60eb054c038
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2010-10-05 12:58:58 +0100

    spirals

--- exercises/sheet_2/2.11/DrawSpiral.java
+++ exercises/sheet_2/2.11/DrawSpiral.java
@@ -2,42 +2,77 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class DrawSpiral extends Frame {
-  protected static class DrawArea extends Panel {
+
+
+  protected static class DrawArea extends Panel implements AdjustmentListener {
+        
     public void paint(Graphics g) {
-      int a = 5;
-      int b = 6;
       
-      int x = 0;
-      while(x < WIDTH) {
-        int y = 0;
-        while( y < height) {
-          int[] coords = archimedean(x,y,a,b);
-          drawPoint(coords[0], coords[1]);
-          y++;
-        }
-        x++;
+      Color oldColor = g.getColor();
+      g.setColor(getBackground());
+      g.drawRect(0,0, WIDTH, HEIGHT);
+      g.setColor(oldColor);
+      
+      double t = 0;
+      while(t < Math.PI * rotations) {
+        int[] coords = archimedian(spacing, t); 
+        g.drawLine(150+ coords[0], 150+ coords[1], 150+ coords[0], 150+coords[1]);
+        t+=step;
       }  
     }
 
-    public int[] archimedian(int x, int y, int a, int b) {
-       double[] polar = cart2pol(x, y);
-       polar[0] = a + b * polar[1];
-       return(pol2cart(polar[0], polar[1]));
+    public void adjustmentValueChanged(AdjustmentEvent e) {
+      Adjustable adjustable = e.getAdjustable();
+      int val = e.getValue();
+      if (adjustable == stepSlider) {
+        step = val * DrawArea.STEP_RESOLUTION;
+      } else if (adjustable == rotationsSlider) {
+        rotations = val;
+      } else if (adjustable == spacingSlider) {
+        spacing = val * DrawArea.SPACING_RESOLUTION;
+      }
+      repaint();
     }
 
-    public double[] cart2pol(int x, int y) {
-      int[] retval = {Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)), Math.atan(x/y)};
-      return(retval);
-    }
 
-    public int[] pol2cart(double r, double theta) {
-      int[] retval = {Math.sin(theta) * r, Math.cos(theta) * r};
-      return (retval);
+    public int[] archimedian(double a, double t) {
+      int[] retval = {(int) Math.floor(a *t * Math.cos(t)), (int) Math.floor(a * t* Math.sin(t))};
+      return retval; 
     }
 
     public Dimension getPreferredSize() {
       return new Dimension(WIDTH, HEIGHT); 
     }
+    
+    public DrawArea() { 
+      super();
+      rotations  = DrawArea.DEFAULT_ROTATIONS;
+      spacing = DrawArea.DEFAULT_SPACING;
+      step = DrawArea.DEFAULT_STEP;
+    };
+
+    public int rotations;
+    public double spacing;
+    public double step;
+
+    public static final int MAX_ROTATIONS = 15;
+    public static final int MIN_ROTATIONS = 1;
+    public static final int DEFAULT_ROTATIONS = 10;
+
+    public static final double MAX_SPACING = 20.0;
+    public static final double MIN_SPACING = 0.0;
+    public static final double DEFAULT_SPACING = 5.0;
+    public static final double SPACING_RESOLUTION = 0.01;
+
+    public static final double MAX_STEP = 0.1;
+    public static final double MIN_STEP = 0.001;
+    public static final double STEP_RESOLUTION = 0.001;
+    public static final double DEFAULT_STEP = 0.001; 
+
+    public Scrollbar rotationsSlider;
+    public Scrollbar spacingSlider;
+    public Scrollbar stepSlider;
+
 
     private int WIDTH =300;
     private int HEIGHT = 300;
@@ -56,10 +91,24 @@ public class DrawSpiral extends Frame {
     DrawArea drawing = new DrawArea();
     Panel buttonPanel = new Panel();
     Button quitButton = new Button("Quit");
-    buttonPanel.add("Center", quitButton);
+
+    drawing.spacingSlider = new Scrollbar(Scrollbar.HORIZONTAL, (int) (DrawArea.DEFAULT_SPACING / DrawArea.SPACING_RESOLUTION), 1,  (int) (DrawArea.MIN_SPACING / DrawArea.SPACING_RESOLUTION),  (int) (DrawArea.MAX_SPACING / DrawArea.SPACING_RESOLUTION));
+
+    drawing.stepSlider = new Scrollbar(Scrollbar.HORIZONTAL, (int) (DrawArea.DEFAULT_STEP / DrawArea.STEP_RESOLUTION), 1,  (int) (DrawArea.MIN_STEP / DrawArea.STEP_RESOLUTION),  (int) (DrawArea.MAX_SPACING / DrawArea.STEP_RESOLUTION));
+
+    drawing.rotationsSlider = new Scrollbar(Scrollbar.HORIZONTAL, DrawArea.DEFAULT_ROTATIONS, 1, DrawArea.MIN_ROTATIONS,  DrawArea.MAX_ROTATIONS);
+    
+    buttonPanel.add("South", quitButton);
+    buttonPanel.add("Center", drawing.spacingSlider);
+    buttonPanel.add("Center", drawing.rotationsSlider);
+    buttonPanel.add("Center",  drawing.stepSlider);
     frame.setLayout(new BorderLayout());
     frame.add("Center", drawing);
     frame.add("South", buttonPanel);
+
+    drawing.spacingSlider.addAdjustmentListener(drawing);
+    drawing.rotationsSlider.addAdjustmentListener(drawing);
+    drawing.stepSlider.addAdjustmentListener(drawing);
 
     quitButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
